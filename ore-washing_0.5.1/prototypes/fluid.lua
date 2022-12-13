82596037f3acf0bddb7ba356adac2ae3a6c3fccc
Walker Dick <walkerraydick2@gmail.com>
2022-12-13 16:45:03 -0600

    version 0.5.1

---
+++ ore-washing_0.5.1/prototypes/fluid.lua
@@ -0,0 +1,14 @@
+data:extend{(
+    {
+        type = "fluid",
+        name = "ore-polish",
+        default_temperature = 25,
+        heat_capacity = "0.1KJ",
+        base_color = {r=0.75, g=0.65, b=0.1},
+        flow_color = {r=0.7, g=.5, b=0.1},
+        icon = "__base__/graphics/icons/fluid/sulfuric-acid.png",
+        tint = {r=128/255, g=0/255, b=0/255},
+        icon_size = 64,
+        order = "a[fluid]-f[ore-polish]"
+      }
+)}
---
+++ ore-washing_0.5.1/prototypes/item.lua
@@ -0,0 +1,135 @@
+data:extend({
+    {
+        type="item",
+        name="ore-polishing-crystal",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/stone.png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/stone.png",   scale = 0.25, mipmap_count = 4 ,tint = {r=229/255, g=224/255, b=217/255,a=20/255}},
+      { size = 64, filename = "__base__/graphics/icons/stone-1.png", scale = 0.25, mipmap_count = 4 ,tint = {r=229/255, g=224/255, b=217/255,a=20/255}},
+      { size = 64, filename = "__base__/graphics/icons/stone-2.png", scale = 0.25, mipmap_count = 4 ,tint = {r=229/255, g=224/255, b=217/255,a=20/255}},
+      { size = 64, filename = "__base__/graphics/icons/stone-3.png", scale = 0.25, mipmap_count = 4 ,tint = {r=229/255, g=224/255, b=217/255,a=20/255}}
+    },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255}
+    },
+    {
+        type="item",
+        name="hardened-ore-polishing-crystal",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/stone-brick.png",
+        icon_size = 64,
+        icon_mipmaps =4,
+        pictures = {
+            {size = 64, filename = "__base__/graphics/icons/stone-brick.png", scale = 0.25,mipmap_count=4,tint = {r=229/255, g=224/255, b=217/255,a=20/255}}
+        },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255,a=22/255}
+    },
+    {
+        type="item",
+        name="tier-1-washed-copper-ore",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/copper-ore.png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/copper-ore.png",   scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}},
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-1.png", scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}},
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-2.png", scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}},
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-3.png", scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}}
+    },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255}
+    },
+    {
+        type="item",
+        name="tier-2-washed-copper-ore",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/copper-ore.png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/copper-ore.png",   scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}},
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-1.png", scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}},
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-2.png", scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}},
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-3.png", scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}}
+    },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255}
+    },
+    {
+        type="item",
+        name="tier-3-washed-copper-ore",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/copper-ore.png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/copper-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__base__/graphics/icons/copper-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255}
+    },
+    {
+        type="item",
+        name="tier-1-washed-iron-ore",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/iron-ore.png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/iron-ore.png",   scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}},
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-1.png", scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}},
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-2.png", scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}},
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-3.png", scale = 0.25, mipmap_count = 4 ,tint = {r=81/255, g=81/255, b=81/255}}
+    },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255}
+    },
+    {
+        type="item",
+        name="tier-2-washed-iron-ore",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/iron-ore.png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/iron-ore.png",   scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}},
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-1.png", scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}},
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-2.png", scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}},
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-3.png", scale = 0.25, mipmap_count = 4 ,tint = {r=135/255, g=135/255, b=135/255}}
+    },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255}
+    },
+    {
+        type="item",
+        name="tier-3-washed-iron-ore",
+        stack_size = 100,
+        icon = "__base__/graphics/icons/iron-ore.png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/iron-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__base__/graphics/icons/iron-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+        subgroup = "intermediate-product",
+        tint = {r=128/255, g=0/255, b=0/255}
+    }
+
+})
